1f9b26652eecea727d2531095e3b0827d532f060
lisianthus <lisianthus623@gmail.com>
2020-01-01 10:37:19 +0700

    adding single player

---
+++ Selection.py
@@ -0,0 +1,12 @@
+import sys
+from os import system
+import loaders as loader
+# from Game.Single import Single
+# from Game.Multiplayer import Multiplayer
+
+class Selection :
+    
+    # Selected single player
+    def Single_player(is_play, play_type) :
+        print("single player")
+
