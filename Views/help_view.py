24b2a8148472e65b53f4ad82a50737782c2ae85e
lisianthus <lisianthus623@gmail.com>
2020-01-15 23:44:22 +0700

    view for help screen

---
+++ Views/help_view.py
@@ -0,0 +1,7 @@
+def help_view() :
+    print("\n[======= Help =======]\n")
+    print("■-------- Help --------■")
+    print("|                      |")
+    print("| Just type the, tho   |")
+    print("|                      |\n")
+    print("Exit [y]")
