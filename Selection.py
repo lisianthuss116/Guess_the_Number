61a0defa83b096dfb0bc19405b481c5efcdb0f3a
lisianthus <lisianthus623@gmail.com>
2020-01-17 08:58:03 +0700

    change single and multiplayer code structure and move some method

--- Selection.py
+++ Selection.py
@@ -1,109 +1,77 @@
-import names
+import os, sys
 import Menu as menus
 import loaders as loader
 from Game.Single import Single
-from Views.singleplayer_view import singlePlayerview
-from Views.multiplayer_view import multiplayer_view
+from Player.Player import Player
+import Views.Menus as Menu
 # from Game.Multiplayer import Multiplayer
 
 class Selection :
     
     # Selected single player
-    def Single_player(is_play, play_type) :
-        singlePlayerview()
-        difficulity = input(">> ")
-        difficulity = str(difficulity.lower().strip())
+    def SinglePlayer(is_play) :
+        # check if player is play
+        if is_play :
 
-        name = Selection.player_name(play_type = "singleplayer", players = None)
-        if name :
-            Selection.Difficulity(difficulity, name)
+            # display single-player menu
+            Menu.MenuView.singlePlayerview()
+
+            # player choice difficulity
+            difficulity = input(">> ")
+            difficulity = int(difficulity.lower().strip())
+
+            # player create name
+            name = Player.set_player_name(
+                game_mode = "singleplayer", 
+                players = None)
+            
+            # check if player has create name
+            if name :
+                # direct to check-difficulity
+                Selection.Difficulity(difficulity, name)
 
     # Selected multiplayer
-    def Multiplayer(is_play, play_type) :
-        multiplayer_view()
-        players = input(">> ")
-        players = str(players.lower().strip())
-        
-        if players == "1" :
-            loader.time_sleep(2, 3, True)
-            print("[=== Really ?, No. u can't play alone in multiplayer, do you have any friends dude ? ===]\n")
-            print("# Back [y/n]")
-            back = input(">> ")
-            if str(back.lower().strip()) == "y" :
-                loader.loading_message()
-                Selection.Multiplayer(is_play = True, play_type = 2)
+    def Multiplayer(is_play) :
+        # check if player is play
+        if is_play :
 
-        name = Selection.player_name(
-            play_type = "multiplayer", 
-            players = players)
-        if name :
-            Selection.CheckPlayers(players, name)
+            # display multiplayer menu
+            Menu.MenuView.multiplayer_view()
+
+            # all player create name
+            name = Selection.set_player_name(
+                game_mode = "multiplayer", 
+                players = players)
+
+            # if player has create name direct to check players
+            if name :
+                Player.check_how_many_players(players, name)
     
     # Selected Help
     def Help(is_play) :
+        # check if player is not play
         if not is_play :
-            help_view()
+
+            # diplay help menu 
+            Menu.MenuView.help_view()
             exit = input(">> ")
 
+            # exit
             if str(exit.lower().strip()) == "y":
                 loader.time_sleep(2, 3, True)
                 menus.Menu()
 
+        # if player is play, direct to main menu
         loader.time_sleep(2, 3, True)
         menus.Menu()
 
     @staticmethod
     def Difficulity(difficulity, name) :
         loader.loading_message('green')
-            
-        set_difficulity = int(difficulity)
-        loader.time_sleep(2, 3, True)
-        Single.Hardships(set_difficulity, name)
-                    
-    @staticmethod
-    def CheckPlayers(players, name) :
-        loader.loading_message('green')
 
-        if players == "1" :
-            print("playing with 2 players")
-
-        elif players == "2" :
-            print("playing with 3 players")
-
-        elif players == "3" :
-            print("playing with 4 players")
-
-
-    @staticmethod
-    def player_name(play_type = None, players = None) :
-        if players is None and play_type == "singleplayer":
-            loader.player_loading()
-            print("\n[==========  Player ==========]\n")
-            print("■-------- Player name --------■\n")
-            print(" 1 ) Type name")
-            print(" 2 ) Random\n")
-            choice = input(" >> ")
-            
-            # Check 
-            if choice.lower().strip() == "1" :
-                loader.time_sleep(2, 3, True)
-                print("\n[==========  Player ==========]\n")
-                print("■-------- Player name --------■\n")
-                print("Input your name")
-                name = input(" >> ")
-                name = str.upper(name[0])+str(name[1:None])
-            else :
-                name = names.get_first_name()
-                name = str.upper(name[0])+str(name[1:None])
-
-            return name
-        elif players is not None and play_type == "multiplayer" :
-            loader.player_loading()
-
-            if players == "2" :
-                pass
-            elif players == "3" :
-                pass
-
-            elif players == "4" :
-                pass
+        diff = int(difficulity)
+        hardship = [None,'Easy', 'Normal', 'Hard', 'Master']
+        set_hardship = getattr(Single, f'{hardship[diff]}')
+        
+        set_hardship(name)
+             
