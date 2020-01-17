516e807f1d99c8cd0813b6d1f38b092d1662ecfb
lisianthus <lisianthus623@gmail.com>
2020-01-17 09:03:37 +0700

    add new loader

--- loaders.py
+++ loaders.py
@@ -3,6 +3,7 @@ import time, random
 from os import system
 from termcolor import colored, cprint
 
+# Loading Start Game [Only when entering the game menu]
 def game_start_message(color = 'green') :
     system("cls")
     print("[====== Entering the Game ======]\n")
@@ -10,13 +11,15 @@ def game_start_message(color = 'green') :
     print("[===============================]")
     time_sleep(1, 3), system("cls")
 
+# Loading player
 def player_loading() :
     system("cls")
     print("[== Preparing ==]\n")
-    cprint(" Please wait ...\n", 'green', file=sys.stderr)
+    cprint(" Please wait.. \n", 'green', file=sys.stderr)
     print("[===============]")  
     time_sleep(1, 3), system("cls")
 
+# Loading message [Check data]
 def loading_message(color = 'green') :
     system("cls")
     print("[==============================]\n")
@@ -24,6 +27,15 @@ def loading_message(color = 'green') :
     print("[==============================]")
     time_sleep(1, 3), system("cls")
 
+# Loading collecting data
+def collecting_data(color = 'green') :
+    system("cls")
+    print("[==============================]\n")
+    cprint(" Please wait, collecting data..\n", color, file=sys.stderr)
+    print("[==============================]")
+    time_sleep(1, 3), system("cls")
+
+# Loading message tips
 def loading_message_tips(color = 'green') :
     system("cls")
     print("[==============================]\n")
@@ -32,6 +44,7 @@ def loading_message_tips(color = 'green') :
     print("[==============================]")
     time_sleep(1, 3), system("cls")
 
+# Load
 def time_sleep(min_val, max_val, clear = False) :
     if clear :
         sleep = time.sleep(random.randint(min_val, max_val)), system("cls")
@@ -40,6 +53,7 @@ def time_sleep(min_val, max_val, clear = False) :
     sleep = time.sleep(random.randint(min_val, max_val))
     return sleep
 
+# Error message
 def error_message(color = 'red') :
     cprint("\n\nThere went error, closing the game", color, file=sys.stderr)
     
