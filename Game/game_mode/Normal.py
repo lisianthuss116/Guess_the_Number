05e7c839a05c402acefe0b4f7925125c9514a75b
lisianthus <lisianthus623@gmail.com>
2020-01-17 09:06:52 +0700

    Normal mode

---
+++ Game/game_mode/Normal.py
@@ -0,0 +1,67 @@
+import os, sys, random  
+sys.path.append('../../')
+import loaders as loader
+from termcolor import colored, cprint
+from Game import Single as SinglePlayer
+from Game.game_mode import estimated_rand as estimate
+
+def Normal(player_guess,player_win,com_guess,com_win,draw,rounds,player) :
+    print("\n[======= Single Player =======]\n")
+    print("■---- Difficulity: Normal ----■")
+    print(f"■----- Player-name: {player}\n")
+
+    while rounds != 6 :
+        rand_num = random.randint(1, 50)
+        loader.time_sleep(1, 2)
+        cprint(f"■■■■------ Round : {str(rounds)} ------■■■■", 'green', file=sys.stderr)
+        print("Guess the number between 1 - 50\n")
+
+        # [ Player and Computer guessing ]
+        
+        # Player
+        print(f"[{player}] Guess the number")
+        player_guess = int(input(">> "))
+
+        # Computer
+        print(f"[Computer] Guess the number")
+        com_guess = estimate.estimated_random_normal_tens(rand_num, com_guess)
+        loader.time_sleep(1, 3)
+        print(">> " + str(com_guess))
+
+        # [ Draw rounds ]
+        if player_guess == rand_num == com_guess :
+            draw = draw + 1
+            com_win = com_win + 1
+            player_win = player_win + 1
+            SinglePlayer.Single.draw_round(rand_num)
+
+        # [ Player win rounds ]
+        elif player_guess == rand_num :
+            player_win = player_win + 1
+            SinglePlayer.Single.player_round_win(player, rand_num)
+
+        # [ Computer win rounds ]
+        elif com_guess == rand_num :
+            com_win = com_win + 1
+            SinglePlayer.Single.com_round_win(rand_num)
+
+        # [ Both wrong ]
+        else :
+            SinglePlayer.Single.wrong(rand_num)
+
+        rounds = rounds + 1
+        ## Rounds Ended ##
+
+    ## Count the numbers of wins
+
+    # [ Draw ]
+    if player_win == com_win :
+        return SinglePlayer.Single.draw(player, player_win, com_win)
+
+    # [ Player win ]
+    elif player_win > com_win :
+        return SinglePlayer.Single.player_win(player, player_win, com_win)
+
+    # [ Computer win ]
+    else :
+        return SinglePlayer.Single.computer_win(player, player_win, com_win)
