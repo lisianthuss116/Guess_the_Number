b5ef6345b67e9b6b10d605ec7b178dc0554f9ad1
lisianthus <lisianthus623@gmail.com>
2020-01-17 14:49:00 +0700

    change unnecessary code

--- Game/game_mode/Normal.py
+++ Game/game_mode/Normal.py
@@ -6,54 +6,52 @@ from Game import Single as SinglePlayer
 from Game.game_mode import estimated_rand as estimate
 
 def Normal(player_guess,player_win,com_guess,com_win,draw,rounds,player) :
-    print("\n[======= Single Player =======]\n")
-    print("■---- Difficulity: Normal ----■")
+    print(f"\n[======= Single Player =======]\n")
+    print(f"■---- Difficulity: Normal ----■")
     print(f"■----- Player-name: {player}\n")
 
     while rounds != 6 :
-        rand_num = random.randint(1, 50)
+        rand_num = random.randint(11, 99)
         loader.time_sleep(1, 2)
         cprint(f"■■■■------ Round : {str(rounds)} ------■■■■", 'green', file=sys.stderr)
-        print("Guess the number between 1 - 50\n")
+        print("Guess the number between 11 - 99\n")
 
         # [ Player and Computer guessing ]
-        
         # Player
         print(f"[{player}] Guess the number")
         player_guess = int(input(">> "))
 
         # Computer
         print(f"[Computer] Guess the number")
-        com_guess = estimate.estimated_random_normal_tens(rand_num, com_guess)
+        com_guess = estimate.estimate_and_set_random(rand_num, rand_num)
         loader.time_sleep(1, 3)
-        print(">> " + str(com_guess))
+        print(f">> {str(com_guess)}")
 
         # [ Draw rounds ]
         if player_guess == rand_num == com_guess :
-            draw = draw + 1
-            com_win = com_win + 1
-            player_win = player_win + 1
+            draw        = draw + 1
+            com_win     = com_win + 1
+            player_win += 1
             SinglePlayer.Single.draw_round(rand_num)
 
         # [ Player win rounds ]
         elif player_guess == rand_num :
-            player_win = player_win + 1
+            player_win += 1
             SinglePlayer.Single.player_round_win(player, rand_num)
 
         # [ Computer win rounds ]
         elif com_guess == rand_num :
-            com_win = com_win + 1
+            com_win += 1
             SinglePlayer.Single.com_round_win(rand_num)
 
         # [ Both wrong ]
         else :
             SinglePlayer.Single.wrong(rand_num)
 
-        rounds = rounds + 1
+        rounds += 1
         ## Rounds Ended ##
 
     ## Count the numbers of wins
-
     # [ Draw ]
     if player_win == com_win :
         return SinglePlayer.Single.draw(player, player_win, com_win)
