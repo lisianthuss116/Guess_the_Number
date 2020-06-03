003d6bc0c735e28e8c7634ca2873c4b4da19ec01
sakuga <lisianthus623@gmail.com>
2020-06-03 19:46:14 +0700

    delete: unnecessary module

--- package/Views/help_view.py
+++
@@ -1,7 +0,0 @@
-def help_view() :
-    print("\n[======= Help =======]\n")
-    print("■-------- Help --------■")
-    print("|                      |")
-    print("| Just type the, tho   |")
-    print("|                      |\n")
-    print("Exit [y]")
--- package/Views/multiplayer_view.py
+++
@@ -1,9 +0,0 @@
-def multiplayer_view() :
-    print("\n[======== Multiplayer ========]\n")
-    print("■---------- Players ----------■")
-    print("|                             |")
-    print("| 1 ) 1 player                |")
-    print("| 2 ) 2 player                |")
-    print("| 3 ) 3 player                |")
-    print("| 4 ) 4 player                |")
-    print("|                             |\n")
