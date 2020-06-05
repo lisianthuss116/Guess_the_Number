7e584152cf440988c08471f445d9be77c4fa808e
sakuga <lisianthus623@gmail.com>
2020-06-05 09:44:40 +0700

    fix: logic

--- src/configurations/_game/services/estimate.py
+++ src/configurations/_game/services/estimate.py
@@ -1,24 +1,32 @@
 import random
+import time
+import random
 
 
-def estimate(mode):
+def estimate(mode, rnumber):
     minmax = {
-        "easy": (1, 10),
-        "normal": (11, 99),
-        "hard": (100, 499),
-        "master": (500, 999)
+        "easy": (1, 10, 5),
+        "normal": (11, 99, 20),
+        "hard": (100, 499, 10),
+        "master": (500, 999, 5)
     }
 
-    xmin, xmax = minmax.get(mode)
+    xmin, xmax, xnum = minmax.get(mode)
 
-    number = random.randint(xmin, xmax)
+    def get_the_closest_value_of_random_number(rnumber, _min, _max, xnum):
+        if rnumber - xnum < 1 or rnumber - xnum < _min:
+            new_min = rnumber
+        else:
+            new_min = abs(rnumber - xnum)
 
-    def get_the_closest_value_of_random_number(number, _min, _max):
-        new_min = number if number - 5 < 1 or number - 5 < _min else abs(number - 5)
-        new_max = number if number + 5 > _max else number + 5
+        if rnumber + xnum > _max:
+            new_max = rnumber
+        else:
+            new_max = rnumber + xnum
 
         new_number = random.randint(new_min, new_max)
 
+        time.sleep(random.randint(1, 3))
         return new_number
 
-    return get_the_closest_value_of_random_number(number, xmin, xmax)
+    return get_the_closest_value_of_random_number(rnumber, xmin, xmax, xnum)
